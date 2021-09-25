6309c167e297908c0e6108c7561a458611eacd0b
Yuriy Mikhanyuk <yurkobyrko@gmail.com>
2021-09-25 21:16:02 +0300

    third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,41 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Варіант завдання №7");
-        Scanner s = new Scanner(System.in);
+        Exercise7 task = new Exercise7();
+        task.Input();
+    }
+
+    static class Exercise7
+    {
         double a, b, x, y, z;
+        Scanner s = new Scanner(System.in);
 
-        System.out.print("Введіть значення a: \n");
-        a = s.nextDouble();
-        System.out.print("Введіть значення b: \n");
-        b = s.nextDouble();
-        System.out.print("Введіть значення x: \n");
-        x = s.nextDouble();
+        void Input()
+        {
+            System.out.println("\nВаріант завдання №7");
 
-        System.out.println("\n1 приклад: ");
-        y = Math.pow(Math.sin(Math.pow((Math.pow(x,2) + a),2)) ,3) - Math.sqrt(x/b);
-        System.out.println("y = " + y);
+            System.out.print("Введіть значення a: ");
+            a = s.nextDouble();
+            System.out.print("Введіть значення b: ");
+            b = s.nextDouble();
+            System.out.print("Введіть значення x: ");
+            x = s.nextDouble();
+            Output();
+        }
 
-        System.out.println("\n2 приклад: ");
-        z = (Math.pow(x, 2) / a) + Math.cos(Math.pow(x + b, 3));
-        System.out.println("z = " + z);
+        void Solve()
+        {
+            y = Math.pow(Math.sin(Math.pow((Math.pow(x,2) + a),2)) ,3) - Math.sqrt(x/b);
+            z = (Math.pow(x, 2) / a) + Math.cos(Math.pow(x + b, 3));
+        }
+
+        void Output()
+        {
+            Solve();
+            System.out.println("\nРозв'язок першого прикладу: ");
+            System.out.println("y = " + y);
+            System.out.println("Розв'язок другого прикладу : ");
+            System.out.println("z = " + z);
+        }
     }
 }
